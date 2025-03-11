2815932e11f052a45734fdb0390819b9a823211d
Dory-noh <no6721@naver.com>
2025-03-11 15:24:28 +0900

    250311
    fish 층별 기준점 일정 범위 내 랜덤 위치 설정
    충돌시 일정 시간 내 한 번만 충돌 체크하도록 설정

---
+++ Assets/02.Scripts/FishLevelCheck.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FishLevelCheck : MonoBehaviour
+{
+    public UnityEvent OnCompareLevel;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
